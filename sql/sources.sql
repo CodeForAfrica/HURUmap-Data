764cb08e688d2fd9dfb019ce8a9153fbee5a9c15
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-03-30 15:52:28 -0230

    Sources table sql

---
+++ sql/sources.sql
@@ -0,0 +1,21 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+ALTER TABLE IF EXISTS ONLY public.sources DROP CONSTRAINT IF EXISTS pk_sources;
+DROP TABLE IF EXISTS public.sources;
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+CREATE TABLE IF NOT EXISTS public.sources (
+	geo_level TEXT,
+	country_code TEXT,
+	table_name TEXT,
+	source_title TEXT,
+	source_link TEXT
+);
