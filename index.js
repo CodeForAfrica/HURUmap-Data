63cf500aa94e1a067c6472a327623721cbaf2fc3
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-04-14 13:02:51 -0230

    Update script to handle different country codes and modified dates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const sequalize = require("./sequalize");
 
-const FILE_COUNTRY_CODE_REGEX = /(?<=_)(za|ke|gh)/;
+const FILE_COUNTRY_CODE_REGEX = /(?<=_)(AF|AX|AL|DZ|AS|AD|AO|AI|AQ|AG|AR|AM|AW|AU|AT|AZ|BS|BH|BD|BB|BY|BE|BZ|BJ|BM|BT|BO|BQ|BA|BW|BV|BR|IO|BN|BG|BF|BI|KH|CM|CA|CV|KY|CF|TD|CL|CN|CX|CC|CO|KM|CD|CG|CK|CR|HR|CU|CW|CY|CZ|CI|DK|DJ|DM|DO|EC|EG|SV|GQ|ER|EE|ET|FK|FO|FJ|FI|FR|GF|PF|TF|GA|GM|GE|DE|GH|GI|GR|GL|GD|GP|GU|GT|GG|GW|GN|GY|HT|HM|VA|HN|HK|HU|IS|IN|ID|IR|IQ|IE|IM|IL|IT|JM|JP|JE|JO|KZ|KE|KI|KP|KR|KW|KG|LA|LV|LB|LS|LR|LY|LI|LT|LU|MO|MK|MG|MW|MY|MV|ML|MT|MH|MQ|MR|MU|YT|MX|FM|MD|MC|MN|ME|MS|MA|MZ|MM|NA|NR|NP|NL|NC|NZ|NI|NE|NG|NU|NF|MP|NO|OM|PK|PW|PS|PA|PG|PY|PE|PH|PN|PL|PT|PR|QA|RO|RU|RW|RE|BL|SH|KN|LC|MF|PM|VC|WS|SM|ST|SA|SN|RS|SC|SL|SG|SX|SK|SI|SB|SO|ZA|GS|SS|ES|LK|SD|SR|SJ|SZ|SE|CH|SY|TW|TJ|TZ|TH|TL|TG|TK|TO|TT|TN|TR|TM|TC|TV|UG|UA|AE|GB|US|UM|UY|UZ|VU|VE|VN|VG|VI|WF|EH|YE|ZM|ZW)\./i;
 
 /** Helpers */
 
@@ -34,8 +34,13 @@ function csv2ObjectsArray(path) {
 
 const transpose = (a) => a[0].map((_, c) => a.map((r) => r[c]));
 
-function combineCSVData(filenames) {
-  return filenames.reduce((combinedData, [filename, countryCode]) => {
+function combineCSVData(tablename) {
+  const filenames = fs.readdirSync("./csv/data").filter(f => f.startsWith(tablename));
+  return filenames.reduce((combinedData, filename) => {
+    const countryCode = (filename.match(FILE_COUNTRY_CODE_REGEX) || [
+      "",
+    ])[0].toLocaleLowerCase();
+
     let data = fs
       .readFileSync(`./csv/data/${filename}`)
       .toString()
@@ -93,9 +98,6 @@ const tables = fs.readdirSync("./csv/data").reduce(
     const tablename = filename.match(FILE_COUNTRY_CODE_REGEX)
       ? filename.slice(0, -7)
       : filename.slice(0, -4); // remove _za.csv or _ke.csv or _gh.csv or .csv
-    const countryCode = (filename.match(FILE_COUNTRY_CODE_REGEX) || [
-      "",
-    ])[0].toLocaleLowerCase();
 
     // Skip files that have already been sequalize
     if (
@@ -106,30 +108,23 @@ const tables = fs.readdirSync("./csv/data").reduce(
       const { mtime: mtimeSQL } = fs.statSync(`./sql/${tablename}.sql`);
 
       const diff = (mtimeSQL.getTime() - mtimeCSV.getTime()) / 1000;
-      if (diff > 0) {
-        merged.skip.push(tablename);
+      if (diff > 0 && merged[tablename] !== 0) {
+        merged[tablename] = 1;
         return merged;
       }
     }
 
-    if (!merged[tablename]) {
-      merged[tablename] = [[filename, countryCode]];
-    } else {
-      merged[tablename].push([filename, countryCode]);
-    }
-
+    merged[tablename] = 0;
     return merged;
   },
-  { skip: [] }
+  { }
 );
 
-const sources = rows2ObjectsArray(combineCSVData(tables["source"]));
+const sources = rows2ObjectsArray(combineCSVData("source"));
 
-Object.entries(tables).forEach(([tablename, filenames]) => {
-  if (!["source", "skip"].includes(tablename)) {
-    const data = combineCSVData(filenames);
-
-    sequalize({ tablename, data, geos, mappedGeos, sources });
+Object.entries(tables).forEach(([tablename, skipped]) => {
+  if (!skipped && !["source"].includes(tablename)) {
+    sequalize({ tablename, data: combineCSVData(tablename), geos, mappedGeos, sources });
   }
 });
 
@@ -161,7 +156,7 @@ const drop = fs
   })
   .filter(
     (tablename) =>
-      tablename && !tables[tablename] && !tables.skip.includes(tablename)
+      tablename && tables[tablename] === undefined
   );
 
 sequalize({ drop });
